8cde9e93c6edecd89bec69f748b6943e68ccfbd7
DanFiz <titarenko.di@phystech.edu>
2023-09-29 15:22:22 +0300

    'sdf'

--- sem3/task6.py
+++ sem3/task6.py
@@ -1,12 +1,21 @@
 import numpy as np
-n=int(input())
-x=[int(input()) for i in range(n)]
-y=[int(input()) for i in range(n)]
-def mnk(x,y):
-    A = np.vstack([x, np.ones(len(x))]).T
-    k, b = np.linalg.lstsq(A, y, rcond=None)[0]
-    return k,b
-B=mnk(x,y)
-k=B[0]
-b=B[1]
-print(f'y={k}x+{b}')
+ln=int(input())
+k=1
+x1=[]
+y1=[]
+while k<=ln:
+    x1.append(float(input()))
+    y1.append(float(input()))
+    k+=1
+x=np.array(x1)
+y=np.array(y1)
+x_mean = np.mean(x)
+y_mean = np.mean(y)
+numerator = (x - x_mean)*y
+denominator = (x - x_mean)**2
+k = np.sum(numerator)/np.sum(denominator)
+b = y_mean - k*x_mean
+twonum=(y-k*x-b)**2
+srkv=np.sum(twonum)
+error=np.sqrt(srkv/len(x))
+print(f'(y = {k}*x + {b})+-{error}')
